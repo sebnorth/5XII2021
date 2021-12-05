949139cc4cd56974c73d08da829875ace2f5e4f9
sebnorth <sebnorth@gmail.com>
2021-12-05 11:57:59 +0100

    Dodaj pliki projektów.

---
+++ Zadanie5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dlug = 80;
+            int dziennyZarobekJanka = 50;
+            int dziennyZarobekKarola = 40;
+            double aktualnieSplaconyDlug = 0;
+            int dzien = 0;
+
+            do
+            {
+                dzien++;
+                aktualnieSplaconyDlug += (0.2 * dziennyZarobekKarola + 0.2 * dziennyZarobekJanka);
+                Console.WriteLine("dzień nr: {0}, spłata łączna do tego dnia: {1}", dzien, aktualnieSplaconyDlug);
+            } while (aktualnieSplaconyDlug < dlug);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  wersja 1D
+             *  uzytkownik podaje n
+             *  program wypisuje bez odstępów n znaków "*"
+             *  
+             *  wersja 2D
+             *  poprzedni kod obejmujemy w pętli zewnętrznej
+             *  
+             *  i - numer wiersza
+             *  j - numer kolumny
+             */
+
+            Console.WriteLine("Podaj rozmiar bok kwadratu: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); 
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
