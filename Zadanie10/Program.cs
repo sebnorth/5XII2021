496b9c511dfd0c7198ac156ecfa4f4f618ddaec8
sebnorth <sebnorth@gmail.com>
2021-12-05 12:51:53 +0100

    Dodano 10 i 11

---
+++ Zadanie10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj rozmiar bok kwadratu: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) // piątemu wierszowi odpowiada i = 4
+            {
+                for (int j = 0; j <= i; j++) // pierwszej kolumnie odpowiada j = 0
+                {
+                    Console.Write("{0,4} ", (i+1)*(j+1));
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * s - ilość spacji
+             * k - ilość krzyżyków
+             * s+k = n
+             */
+            Console.WriteLine("Podaj rozmiar bok kwadratu: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) // i - numer wiersza = k
+            {
+
+                int z;
+                // ustaw n-k = n-i spacji
+                for (z = 0; z < n-i; z++)
+                {
+                    Console.Write(" ");
+                }
+
+                // dostaw i krzyżyków
+                for (z=0; z < i; z++)
+                {
+                    Console.Write("X");
+                }
+
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
